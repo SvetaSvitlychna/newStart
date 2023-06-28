6ecb6645b145d0848ee1afde37ad52401f20f9f4
SvetaSvitlychna <manulwild@gmail.com>
2023-06-28 18:33:49 +0300

    all

--- homePage.php
+++ homePage.php
@@ -1077,7 +1077,7 @@ echo sum($arr);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>mainPageCheckout</title>
+    <title>mainPageCheckout34</title>
 </head>
 <br>
 <p>Main page</p>
